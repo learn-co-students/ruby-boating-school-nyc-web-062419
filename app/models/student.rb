4669b715961e3442b90b137175ea99fe9c01db3c
nicholasstano <nicholasstano@gmail.com>
2019-07-01 09:06:05 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Student
+
+	attr_reader :first_name
+
+	@@all = []
+
+	def initialize(first_name)
+		#should initialize with first_name
+		@first_name = first_name
+		Student.all << self
+	end
+
+	def self.all 
+		#Student.all should return all of the student instances
+		@@all 
+	end
+
+	def self.find_student(name)
+		# Student.find_student will take in a first name and output the student (Object) with that name
+		Student.all.find do |students|
+			name == students.first_name
+		end
+	end
+
+	def add_boating_test(test_n, test_s, ins)
+		# Student#add_boating_test should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+		BoatingTest.new(self, test_n, test_s, ins)
+	end
+
+	def all_boating_tests
+		BoatingTest.all.select do |boating_test_instance|
+			boating_test_instance.student == self
+		end
+	end
+
+	def grade_percentage
+		# Student#grade_percentage should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float 33.33)
+		a = self.all_boating_tests.collect do |information|
+			information.test_status == "passed"
+		end
+		b = a.count(true) / a.size.to_f * 100
+		b.round(2)
+	end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,34 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ark = Student.new("Ark")
+bry = Student.new("Bry")
+ced = Student.new("Ced")
+drl = Student.new("Drl")
+ele = Student.new("Ele")
+frk = Student.new("Frk")
+gax = Student.new("Gax")
+hoy = Student.new("Hoy")
+
+izy = Instructor.new("Izy")
+igy = Instructor.new("Igy")
+jre = Instructor.new("Jre")
+kly = Instructor.new("Kly")
+
+tst1 = BoatingTest.new(ark, "TST", "failed", izy)
+tst2 = BoatingTest.new(drl, "TST", "pending", kly)
+tst3 = BoatingTest.new(ele, "TST", "passed", igy)
+tzt1 = BoatingTest.new(bry, "TZT", "failed", igy)
+tzt2 = BoatingTest.new(frk, "TZT", "pending", jre)
+tzt3 = BoatingTest.new(gax, "TZT", "passing", kly)
+tnt1 = BoatingTest.new(ced, "TNT", "failed", jre)
+tnt2 = BoatingTest.new(ark, "TNT", "pending", jre)
+tnt3 = BoatingTest.new(bry, "TNT", "passed", igy)
+tpt1 = BoatingTest.new(bry, "TPT", "failed", kly)
+tpt2 = BoatingTest.new(hoy, "TPT", "pending", igy)
+tpt3 = BoatingTest.new(ark, "TPT", "passed", jre)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
