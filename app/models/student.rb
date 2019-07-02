c169f6af033719726f7237e5969ce06da9847bb6
thenatural86 <philip.smalls86@gmail.com>
2019-07-02 09:37:19 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Student
+  attr_accessor :first_name
+
+  @@all = []
+  
+  def initialize(first_name)
+    @first_name = first_name
+    Student.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(name, status, instructor)
+    BoatingTest.new(self, name, status, instructor)
+  end
+  
+  def self.find_student(student_first_name)
+    self.all.find do |student|
+      student.first_name == student_first_name
+    end
+  end
+
+  # * `Student#grade_percentage` should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float `33.33`)
+
+  def grade_percentage
+  end
 
 end
