983b973e9064e30e9a97711692ca0aa5bd79ae4b
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-02 09:48:31 -0400

    what i learned at boating school is

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,30 @@
 class Student
 
+    @@all = []
+
+    attr_reader :first_name
+
+    def initialize (first_name)
+        @first_name = first_name
+        Student.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(self, test_name, status, instructor)
+    end
+
+    def self.find_student(first_name)
+        self.all.find {|student| student.first_name == first_name}
+    end
+
+    def grade_percentage
+        my_tests = BoatingTest.all.select {|test| test.student == self}
+        passing = my_tests.select{|test| test.status == "passed"}
+        ((passing.length.to_f / my_tests.length) * 100).round(2)
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+ 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+test1 = spongebob.add_boating_test("test1", "failed", puff)
+test2 = spongebob.add_boating_test("test2", "passed", krabs)
+test3 = spongebob.add_boating_test("test3", "failed", puff)
+test4 = patrick.add_boating_test("test4", "passed", krabs)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
