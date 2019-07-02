cded4d15a95cd1867623b0924c1ca73ed0bac2d5
yoseflehrer <yolehrer@gmail.com>
2019-07-02 09:08:36 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,14 @@
 class BoatingTest
 
+    @@all = []
+    attr_accessor :test_name, :status, :student
+    def initialize(student, test_name, status, instructor)
+        @test_name = test_name
+        @status = status
+        BoatingTest.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,22 @@
 class Student
 
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        Student.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, status, instructor_object)
+        BoatingTest.new(self, test_name, status, instructor_object)
+    end
+
+    def self.find_student(given_name)
+        self.all.find {|stu| stu.name == given_name}
+    end
+
 end
