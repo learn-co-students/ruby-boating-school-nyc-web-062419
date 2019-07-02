79e9bfb7c1050f0ff1c6104688b2234bb5beb2a7
Anthony Wan <Anthonywan00@gmail.com>
2019-07-02 10:09:33 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,17 @@
 class BoatingTest
 
+    attr_reader :student, :test_name, :instructor
+    attr_accessor :status
+    @@all = []
+    def initialize student,test_name,instructor
+        @student = student
+        @test_name = test_name
+        @instructor = instructor
+        @status = "pending"
+        BoatingTest.all << self
+    end
+
+    def self.all
+        @@all 
+    end
 end
